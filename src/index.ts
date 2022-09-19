26d7013d940f2e3033b1b5d55ceba8222590e1ac
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-19 13:55:11 -0400

    type assersation notes and html input update

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ printDouble('Hello World! :D! ');
 
 console.log("hello".replaceAll("h", "`")); //without the lib option, we will not be able to do this. See tsconfig or readme for details. 
 
+// //Type Assertions v telling TS, "i know more than you."
+// let mystery: unknown = "Solving the mystery"; 
+
+// const numChars = (mystery as string).length; 
 
 const btn = document.getElementById("btn");
 console.log("SHOW BUTTON >>", btn); 
