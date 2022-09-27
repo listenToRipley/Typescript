92445b9dbc913c77f2f383af096e5d8d7bf76ded
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-27 14:22:17 -0400

    missing lite-serve, added

---
+++ GENERICS/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const nums = [];
+const color = [];
+const inputEl = document.querySelector("#username");
+console.log(inputEl);
+console.dir(inputEl);
+inputEl.value = "Update the text field";
