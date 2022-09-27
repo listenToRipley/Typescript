0a7ff9f2d4d7d497855c5881203c467ecc891c59
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-27 14:57:30 -0400

    generic created

--- GENERICS/dist/index.js
+++ GENERICS/dist/index.js
@@ -5,3 +5,25 @@ const inputEl = document.querySelector("#username");
 console.log(inputEl);
 console.dir(inputEl);
 inputEl.value = "Update the text field";
+const btn = document.querySelector(".btn");
+function numberIdentity(item) {
+    return item;
+}
+;
+function stringIdentity(item) {
+    return item;
+}
+;
+function booleanIdentity(item) {
+    return item;
+}
+;
+;
+function identity(item) {
+    return item;
+}
+;
+identity('Returns a string');
+identity(7);
+identity(true);
+identity({ name: 'moew', breed: 'house-cat' });
--- GENERICS/src/index.ts
+++ GENERICS/src/index.ts
@@ -8,3 +8,37 @@ const inputEl = document.querySelector<HTMLInputElement>("#username")!;
 console.log(inputEl);
 console.dir(inputEl); 
 inputEl.value = "Update the text field"; 
+
+const btn = document.querySelector<HTMLButtonElement>(".btn");
+
+//making your own generic 
+function numberIdentity(item:number):number {
+  return item; 
+}; 
+
+function stringIdentity(item:string):string {
+  return item; 
+}; 
+
+function booleanIdentity(item:boolean):boolean {
+  return item; 
+}; 
+
+// function identity(item:any):any { //give anything return anything. 
+//   return item; 
+// }; 
+
+interface Cat {
+  name: string;
+  breed: string;
+};
+
+function identity<Type>(item:Type): Type { // we establish a relationship that show what information will be coming in and going out. 
+  return item; // usually you will see an T in stead of Type.
+};
+
+identity<string>('Returns a string');
+identity<number>(7);
+identity<boolean>(true);
+
+identity<Cat>({name:'moew', breed:'house-cat'}); // cat goes in, and cat comes out. 
