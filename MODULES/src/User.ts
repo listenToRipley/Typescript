8df59cd6f8538c5b451a794d2f09f887e6a2c098
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-29 14:15:51 -0400

    modules completed

--- MODULES/src/User.ts
+++ MODULES/src/User.ts
@@ -1,4 +1,4 @@
-import type { Person } from "./types.js";
+import type { Person } from "./types.js"; // is a safer way to import and will be more safely removed from a compiler
 export default class User implements Person {
   constructor(public username: string, public email: string) {}
   logout() {
--- MODULES/src/index.ts
+++ MODULES/src/index.ts
@@ -1,6 +1,9 @@
 import { add, sample as randomSample, pi } from "./utils.js";
+// as  is a way to provide an alias.
 import User, { userHelper } from "./User.js";
 const sample = 12234233;
 console.log(randomSample([12, 3, 34]));
 console.log(add(1, 2));
 console.log(pi);
+console.log(User);
+console.log(userHelper); //standard exports 
