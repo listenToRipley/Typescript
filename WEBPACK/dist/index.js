dae71571b974643997b413faa6186d5b9f1ea502
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-29 16:13:00 -0400

    more typos

--- WEBPACK/dist/index.js
+++ WEBPACK/dist/index.js
@@ -1,9 +1,9 @@
-import Dog from './Dog.js';
-import { add, multiply, divide } from './utils';
-import ShelterDog from './ShelterDog';
-console.log("From index file");
-const gruff = new Dog("Gruff", "Pure Breed Good Boy", 5);
-console.log(`Gruff, "${gruff.bark()}"`);
+import Dog from "./Dog.js";
+import ShelterDog from "./ShelterDog.js";
+import { add, multiply, divide } from "./utils.js";
+console.log("FROM INDEX FILE!!!!!!!!");
+const gruff = new Dog("Gruff", "Pure Bred Good Boy", 5);
+console.log(`${gruff.name}, says "${gruff.bark()}"`);
 console.log(add(4, 5));
 console.log(multiply(4, 5));
 console.log(divide(4, 5));
--- WEBPACK/src/index.ts
+++ WEBPACK/src/index.ts
@@ -1,14 +1,15 @@
-import Dog from './Dog.js'; 
-import { add, multiply, divide } from './utils';
-import ShelterDog from './ShelterDog';
+import Dog from "./Dog";
+import ShelterDog from "./ShelterDog";
+import { add, multiply, divide } from "./utils";
+console.log("FROM INDEX FILE!!!!!!!!");
 
-console.log("From index file");
-const gruff = new Dog("Gruff", "Pure Breed Good Boy", 5);
-console.log(`Gruff, "${gruff.bark()}"`); 
+const gruff = new Dog("Gruff", "Pure Bred Good Boy", 5);
+console.log(`${gruff.name}, says "${gruff.bark()}"`)
 
-console.log(add(4,5));
-console.log(multiply(4,5));
-console.log(divide(4,5));
+console.log(add(4, 5));
+console.log(multiply(4, 5));
+console.log(divide(4, 5));
+
 
 const dreamDog = new ShelterDog("Melon", "Terrier", 3, "Rochester Animal Shelter");
 console.log(`${dreamDog.name}, says "${dreamDog.bark()}"`)
