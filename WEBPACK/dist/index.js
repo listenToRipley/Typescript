d10656dd665afde6c47f2929c51e8f74b6a31d83
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-29 15:58:21 -0400

    update modules  config and added shelter dog

---
+++ WEBPACK/dist/index.js
@@ -0,0 +1,11 @@
+import Dog from './Dog.js';
+import { add, multiply, divide } from './utiles';
+import ShelterDog from './ShelterDog';
+console.log("From index file");
+const gruff = new Dog("Gruff", "Pure Breed Good Boy", 5);
+console.log(`Gruff, "${gruff.bark()}"`);
+console.log(add(4, 5));
+console.log(multiply(4, 5));
+console.log(divide(4, 5));
+const dreamDog = new ShelterDog("Melon", "Terrier", 3, "Rochester Animal Shelter");
+console.log(`${dreamDog.name}, says "${dreamDog.bark()}"`);
