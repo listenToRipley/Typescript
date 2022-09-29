4031fc192323778b16eb3b8fc0158379640e2809
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-29 15:44:11 -0400

    update

--- WEBPCK/src/index.ts
+++ WEBPCK/src/index.ts
@@ -1,1 +1,10 @@
+import Dog from './Dog.js'; 
+import { add, multiply, divide } from './utiles';
+
 console.log("From index file");
+const gruff = new Dog("Gruff", "Pure Breed Good Boy", 5);
+console.log(`Gruff, "${gruff.bark()}"`); 
+
+console.log(add(4,5));
+console.log(multiply(4,5));
+console.log(divide(4,5));
