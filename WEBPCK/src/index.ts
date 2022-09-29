5cf5ba2afa727864632fe70262450194a61b557a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-29 15:52:32 -0400

    created shelter dog

--- WEBPCK/src/index.ts
+++ WEBPCK/src/index.ts
@@ -1,5 +1,6 @@
 import Dog from './Dog.js'; 
 import { add, multiply, divide } from './utiles';
+import ShelterDog from './ShelterDog';
 
 console.log("From index file");
 const gruff = new Dog("Gruff", "Pure Breed Good Boy", 5);
@@ -8,3 +9,6 @@ console.log(`Gruff, "${gruff.bark()}"`);
 console.log(add(4,5));
 console.log(multiply(4,5));
 console.log(divide(4,5));
+
+const dreamDog = new ShelterDog("Melon", "Terrier", 3, "Rochester Animal Shelter");
+console.log(`${dreamDog.name}, says "${dreamDog.bark()}"`)
