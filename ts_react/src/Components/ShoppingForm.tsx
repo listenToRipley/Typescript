590b94d16b4b8d25e940705550f796e16da4f68b
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-02 16:08:43 -0400

    handler

--- ts_react/src/Components/ShoppingForm.tsx
+++ ts_react/src/Components/ShoppingForm.tsx
@@ -1,8 +1,14 @@
 import React from 'react'; 
 
 export default function ShoppingForm() {
+  
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault(); 
+    console.log(`hello, ${event}`)
+
+  }
   return <div>
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Product Name" />
       <button type="submit">Add Item</button>
     </form>
