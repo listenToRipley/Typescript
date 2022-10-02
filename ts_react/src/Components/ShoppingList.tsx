1acd96fa7fede68225c7773e7664f59767d3b8cf
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-02 15:09:22 -0400

    mapping items

--- ts_react/src/Components/ShoppingList.tsx
+++ ts_react/src/Components/ShoppingList.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 function ShoppingList(): JSX.Element {
 
+  const items = [
+    {id: 1, product:"Lemon", quantity: 3},
+    {id: 2, product:"Chili Pepper", quantity: 1},
+    {id: 3, product:"Whole Chicken", quantity: 1}
+  ]
+
   return <div>
     <h1>Shopping List</h1>
     <ul>
-      <li>Item 1</li>
+      {items.map((item) => {
+        return <li key={item.id}><span>{item.product}</span> - <span>{item.quantity}</span></li>
+      })}
     </ul>
   </div>
 
