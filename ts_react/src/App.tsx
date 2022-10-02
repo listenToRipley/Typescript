d1c1f8e8146661c400421a82debeb90e1136668b
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-02 15:25:37 -0400

    alterative syntax

--- ts_react/src/App.tsx
+++ ts_react/src/App.tsx
@@ -5,10 +5,19 @@ import Greeter from './Components/Greeter';
 import ShoppingList from './Components/ShoppingList';
 
 function App() {
+
+
+  const items = [
+    {id: 1, product:"Lemon", quantity: 3},
+    {id: 2, product:"Chili Pepper", quantity: 1},
+    {id: 3, product:"Whole Chicken", quantity: 1}
+  ]
+
+
   return (
     <div>
       <Greeter/> 
-      <ShoppingList/>
+      <ShoppingList items={items}/>
     </div>
   );
 }
--- ts_react/src/Components/ShoppingList.tsx
+++ ts_react/src/Components/ShoppingList.tsx
@@ -1,16 +1,38 @@
 import React from 'react'; 
 
-function ShoppingList(): JSX.Element {
+interface Items { // this is the array of objects 
+  id: number; 
+  product: string;
+  quantity: number; 
 
-  const items = [
-    {id: 1, product:"Lemon", quantity: 3},
-    {id: 2, product:"Chili Pepper", quantity: 1},
-    {id: 3, product:"Whole Chicken", quantity: 1}
-  ]
+} 
 
+interface ShoppingListProps {
+  items: Items[] // the item passed is an array of objects. 
+
+} 
+
+// function ShoppingList(props: ShoppingListProps): JSX.Element {
+//   return <div>
+//     <h1>Shopping List</h1>
+//     <ul>
+//       {/* items its the element passed from the Shopping List interface, which is an array of objects  */}
+//       {props.items.map((item) => {
+//         return <li key={item.id}><span>{item.product}</span> - <span>{item.quantity}</span></li>
+//       })}
+//     </ul>
+//   </div>
+
+// }; 
+
+// alteratively destructure 
+export default function ShoppingList({
+  items,
+}: ShoppingListProps): JSX.Element {
   return <div>
     <h1>Shopping List</h1>
     <ul>
+      {/* items its the element passed from the Shopping List interface, which is an array of objects  */}
       {items.map((item) => {
         return <li key={item.id}><span>{item.product}</span> - <span>{item.quantity}</span></li>
       })}
@@ -19,4 +41,4 @@ function ShoppingList(): JSX.Element {
 
 }; 
 
-export default ShoppingList;
+// export default ShoppingList;
